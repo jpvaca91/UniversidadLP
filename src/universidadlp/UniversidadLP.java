155af8aee6849e07b7b10859658fd8cbc2566a3a
jpvaca91 <vacajuanpablo@gmail.com>
2023-09-12 20:29:51 -0300

    algo

--- src/universidadlp/UniversidadLP.java
+++ src/universidadlp/UniversidadLP.java
@@ -2,14 +2,21 @@
 package universidadlp;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import universidadlp.AccesoADatos.AlumnoData;
 import universidadlp.AccesoADatos.Coneccion;
+import universidadlp.Entidades.Alumno;
 
 
 public class UniversidadLP {
 
    
     public static void main(String[] args) {
-        Connection con=Coneccion.getConexion();
+        
+        Alumno juan=new Alumno(35585789,"Gonzalez","Paula",LocalDate.of(1987,5,21),false);
+        AlumnoData alu= new AlumnoData();
+        alu.guardarAlumno(juan);
+        
         
     }
 
