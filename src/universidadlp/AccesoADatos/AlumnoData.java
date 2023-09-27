b1e2372efa77659efd9a5b849792ebe7cd2097fb
norpablo <norpablocabrera25@gmail.com>
2023-09-27 19:03:21 -0300

    override

--- src/universidadlp/AccesoADatos/AlumnoData.java
+++ src/universidadlp/AccesoADatos/AlumnoData.java
@@ -78,87 +78,82 @@ public class AlumnoData {
         }
         return alumno;
     }
-    
-    public void actualizarAlumno(Alumno alumno){
-      String sql= "UPDATE alumno SET DNI=?, apellido=?, nombre=?, fechaNacimiento=?"
-              + "WHERE DNI=?";
-     
+
+    public void actualizarAlumno(Alumno alumno) {
+        String sql = "UPDATE alumno SET DNI=?, apellido=?, nombre=?, fechaNacimiento=?"
+                + "WHERE DNI=?";
+
         try {
-            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, alumno.getDni());
             ps.setString(2, alumno.getApellido());
             ps.setString(3, alumno.getNombre());
             ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
-            ps.setInt(5,alumno.getDni());
-            
-            int exito=ps.executeUpdate();
-            if (exito==1){
+            ps.setInt(5, alumno.getDni());
+
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
                 JOptionPane.showMessageDialog(null, "Alumno actualizado");
-            
+
             } else {
                 JOptionPane.showMessageDialog(null, "No existe el alumno");
             }
             ps.close();
-            
+
         } catch (SQLException ex) {
-          JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
-         
-          
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+
         }
-        
-        
-        }
-        
-        public void eliminarAlumno (int DNI){
-              String sql="UPDATE alumno SET estado=0 WHERE DNI =?";
-              
+
+    }
+
+    public void eliminarAlumno(int DNI) {
+        String sql = "UPDATE alumno SET estado=0 WHERE DNI =?";
+
         try {
-            PreparedStatement ps=con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, DNI);
-          int exito= ps.executeUpdate();
-            if (exito==1){
-           
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+
                 JOptionPane.showMessageDialog(null, "Alumno eliminado");
-            
-        } else {
+
+            } else {
                 JOptionPane.showMessageDialog(null, "Alumno no encontrado");
             }
-            
-            
-            
-            
+
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Erro al acceder a la tabla alumno");
         }
-        
-              
-          }
-            public List<Alumno> listarAlumnos() {
 
-        String sql = "SELECT DNI,apellido,nombre FROM alumno WHERE estado=1 ";
+    }
+
+    public List<Alumno> listarAlumnos() {
+
+        String sql = "SELECT idAlumno,DNI,apellido,nombre FROM alumno WHERE estado=1 ";
         ArrayList<Alumno> alumnos = new ArrayList<>();
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-           
 
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-               Alumno alumno = new Alumno();
+                Alumno alumno = new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
                 alumno.setDni(rs.getInt("DNI"));
                 alumno.setApellido(rs.getString("apellido"));
                 alumno.setNombre(rs.getString("nombre"));
-                
+
                 alumno.setEstado(true);
-                
+
                 alumnos.add(alumno);
 
-            } 
+            }
             ps.close();
 
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumnos" + ex);
         }
         return alumnos;
-    }  
-    
+    }
+
 }
